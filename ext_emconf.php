8afc51f71531398c6dac2f015a0f4864ec83c408
Lex Frolenko <lex.frolenko.stratis@gmail.com>
2018-12-24 11:16:36 +0200

    wizard_crpagetree > updated to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,51 +1,39 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "wizard_crpagetree".
- *
- * Auto generated 03-10-2013 07:21
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'Web>Func, Wizards, Create page tree',
-	'description' => 'Wizard that will create a page tree for you. Feed it with a space indented tree structure of the desired pages and the pages will be created for you.',
-	'category' => 'module',
-	'shy' => 0,
-	'version' => '1.0.2',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Michiel Roos',
-	'author_email' => 'extensions@donationbasedhosting.org',
-	'author_company' => 'Donation Based Hosting',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'func_wizards' => '',
-			'' => '',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Web>Func, Wizards, Create page tree',
+    'description' => 'Wizard that will create a page tree for you. Feed it with a space indented tree structure of the desired pages and the pages will be created for you.',
+    'category' => 'module',
+    'shy' => 0,
+    'version' => '1.1.0',
+    'dependencies' => 'func',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'autoload' => array(
+        'psr-4' => array('MichielRoos\\WizardCrpagetree\\' => 'Classes'),
+        'classmap' => array('Classes')
+    ),
+    'author' => 'Michiel Roos',
+    'author_email' => 'extensions@donationbasedhosting.org',
+    'author_company' => 'Donation Based Hosting',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+            'func' => '',
+            'typo3' => '9.0.0-9.5.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    'suggests' => array(),
+    '_md5_values_when_last_written' => 's:0:""',
 );
-
-?>
